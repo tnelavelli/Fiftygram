cad7288a09f274d39a5fe76afd9bbe56ae6a5579
Taran Nelavelli <tnelavelli@wisc.edu>
2020-03-26 16:20:22 -0500

    Working ImageView and ChoosePhoto Buttons, working on filters now.

--- app/src/main/java/com/tarannelavelli/fiftygram/MainActivity.java
+++ app/src/main/java/com/tarannelavelli/fiftygram/MainActivity.java
@@ -1,13 +1,63 @@
 package com.tarannelavelli.fiftygram;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private Bitmap image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public void choosePhoto(View v) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            Uri uri = data.getData();
+            try {
+                ParcelFileDescriptor parcelFileDescriptor = getContentResolver().openFileDescriptor(uri, "r");
+                FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+                image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+                parcelFileDescriptor.close();
+                imageView.setImageBitmap(image);
+            } catch (FileNotFoundException e) {
+                Log.e("error", "FileNotFoundException");
+                e.printStackTrace();
+            } catch(IOException e) {
+                Log.e("error", "IOException");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void applySepia(View view) {
+
     }
 }
