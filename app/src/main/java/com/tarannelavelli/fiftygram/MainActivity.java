bda46333162d8e3356f954f101a674b2d45af3a7
Taran Nelavelli <tnelavelli@wisc.edu>
2020-03-26 17:53:02 -0500

    Finished all filtering.

--- app/src/main/java/com/tarannelavelli/fiftygram/MainActivity.java
+++ app/src/main/java/com/tarannelavelli/fiftygram/MainActivity.java
@@ -1,23 +1,38 @@
 package com.tarannelavelli.fiftygram;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.load.Transformation;
+import jp.wasabeef.glide.transformations.gpu.SepiaFilterTransformation;
+import jp.wasabeef.glide.transformations.gpu.SketchFilterTransformation;
+import jp.wasabeef.glide.transformations.gpu.ToonFilterTransformation;
 
-import java.io.FileDescriptor;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
     private ImageView imageView;
     private Bitmap image;
 
@@ -25,8 +40,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
         imageView = findViewById(R.id.imageView);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     public void choosePhoto(View v) {
@@ -47,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
                 image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
                 parcelFileDescriptor.close();
                 imageView.setImageBitmap(image);
+                imageView.setContentDescription("Default");
             } catch (FileNotFoundException e) {
                 Log.e("error", "FileNotFoundException");
                 e.printStackTrace();
@@ -57,7 +78,63 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void applySepia(View view) {
+    public void apply(Transformation<Bitmap> filter) {
+        Glide.with(this)
+                .load(image)
+                .apply(RequestOptions.bitmapTransform(filter))
+                .into(imageView);
+    }
 
+    public void applySepia(View view) {
+        apply(new SepiaFilterTransformation());
+        imageView.setContentDescription("Sepia");
+    }
+
+    public void applyToon(View view) {
+        apply(new ToonFilterTransformation());
+        imageView.setContentDescription("Toon");
+    }
+
+
+    public void applySketch(View view) {
+        apply(new SketchFilterTransformation());
+        imageView.setContentDescription("Sketch");
+    }
+
+    public void savePhoto(View view) throws IOException {
+        Bitmap modifiedImage = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+        boolean saved;
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd");
+        LocalDate localDate = LocalDate.now();
+        OutputStream fos;
+
+        CharSequence charSequence = imageView.getContentDescription();
+        final StringBuilder sb = new StringBuilder(charSequence.length());
+        sb.append(charSequence);
+
+        String name = sb.toString() + "_" + dtf.format(localDate);
+        Log.d("date", name);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ContentResolver resolver = getApplicationContext().getContentResolver();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/png");
+            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, "Pictures/");
+            Uri imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+            fos = resolver.openOutputStream(imageUri);
+        } else {
+            String imagesDir = getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES).toString();
+            File image = new File(imagesDir, name + ".png");
+            fos = new FileOutputStream(image);
+        }
+
+        saved = modifiedImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        fos.flush();
+        fos.close();
+    }
+
+    public void removeFilters(View view) {
+        imageView.setImageBitmap(image);
+        imageView.setContentDescription("Default");
     }
 }
